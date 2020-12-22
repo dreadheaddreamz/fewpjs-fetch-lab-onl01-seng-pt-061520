6e1f3fd1a83d6de0a51a4368538d0cfaaa3e6e0d
James Batsell <jbfuturedev@gmail.com>
2020-12-22 21:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => renderBooks(json))
+const fetchBooks = () => {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(parseJSON)
+    .then(renderBooks)
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-});
+})
+function parseJSON(response) {
+  return response.json();
+}
