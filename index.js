e5b95adadabe161816a2f68283564a88b7e82091
James Batsell <jbfuturedev@gmail.com>
2020-12-22 21:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function renderBooks(json) {
   })
 }
 
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+}))
